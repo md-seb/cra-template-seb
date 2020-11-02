dc7ac0f88b4b570ce3c02d90d61f27c8ced0c786
Seb Pogorzelski <seb@metdesk.com>
2020-11-02 17:42:54 +0000

    fix: reduce the setup to match recent create-react-app

--- template/.eslintrc.js
+++ template/.eslintrc.js
@@ -1,25 +1,13 @@
+// see https://github.com/facebook/create-react-app/tree/master/packages/eslint-config-react-app
 module.exports = {
+  root: true,
   extends: [
-    'eslint:recommended',
-    'plugin:react/recommended', // eslint-plugin-react
-    'plugin:react-hooks/recommended', // eslint-plugin-react-hooks
-    'plugin:jest/recommended', // eslint-plugin-jest
-    'plugin:prettier/recommended', // eslint-plugin-prettier
+    'react-app',
+    'shared-config',
+    'prettier', // eslint-plugin-prettier
     'prettier/react', // support eslint-plugin-react
   ],
   plugins: ['prettier', 'jest', 'react'],
-  env: {
-    browser: true,
-    node: true,
-    es6: true,
-  },
-  parserOptions: {
-    ecmaVersion: 2020, // Allows modern ECMAScript features
-    sourceType: 'module', // Allows use of imports
-    ecmaFeatures: {
-      jsx: true, // Allows parsing of JSX
-    },
-  },
   rules: {
     // Place to specify ESLint rules.
     // Can be used to overwrite rules specified from the extended configs
@@ -31,25 +19,11 @@ module.exports = {
     'react/no-unused-state': 'warn',
     'react/sort-comp': 'warn',
     'react/sort-prop-types': 'warn',
-    'prettier/prettier': 'error',
+    'prettier/prettier': 'warn',
     'jest/no-disabled-tests': 'warn',
     'jest/no-focused-tests': 'error',
     'jest/no-identical-title': 'error',
     'jest/prefer-to-have-length': 'warn',
     'jest/valid-expect': 'error',
-  },
-  overrides: [
-    {
-      files: ['**/*.test.js', '**/*.test.jsx', '**/*.spec.js', '**/*.spec.jsx'],
-      env: {
-        jest: true,
-      },
-    },
-  ],
-  settings: {
-    react: {
-      // Automatically detect React version in eslint-plugin-react
-      version: 'detect',
-    },
   },
 };
